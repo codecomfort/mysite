67fa482350a27f84aa6bb5ca02536771375f94ae
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-03-23 01:32:55 +0900

    コメント修正

--- src/counter/Root.tsx
+++ src/counter/Root.tsx
@@ -7,6 +7,8 @@ import { ActionDispatcher } from "./module";
 // 引数の Counter(これは Redux に依存しない純粋な React コンポーネント) に
 // store と actionDispatcher を差し込んで返すイメージ
 // このように DI することで、Counter コンポーネントは Redux に依存しなくてよくなる
+// また、index.tsx で Provider の store を設定していることで、
+// 右辺の store と dispatch は宣言することなく使用できる
 export default connect(
     (store: any) => ({value: store.counter}),
     (dispatch: Dispatch<any>) => ({actions: new ActionDispatcher(dispatch)})
