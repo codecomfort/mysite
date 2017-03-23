bff24e1cb1e3a384435427e78da3be880d08f8b5
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-03-23 13:07:51 +0900

    TicTacToe用のページをPortfolioへ追加

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import { Route, Link } from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import Counter from './counter/Root';
+import TicTacToe from './react-tutorial-tictactoe/TicTacToe';
 
 const Portfolio = () => (
   <div>
     <h2>Portfolio Page</h2>
     <ul>
-        <li><Link to="/portfolio/counter">Counter</Link></li>
-        <li><Link to="/">Home</Link></li>
+      <li><Link to="/portfolio/counter">Counter</Link></li>
+      <li><Link to="/portfolio/react-tutorial-tictactoe">React Tutorial Tic Tac Toe</Link></li>
+      <li><Link to="/">Home</Link></li>
     </ul>
 
-    <Route path="/portfolio/counter" component={ Counter } />
+    <Route path="/portfolio/counter" component={ Counter }/>
+    <Route path="/portfolio/react-tutorial-tictactoe" component={ TicTacToe }/>
   </div>
 );
 
---
+++ src/react-tutorial-tictactoe/TicTacToe.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Link } from 'react-router-dom';
+
+const TicTacToe  = () => (
+  <div className="tictactoe">
+    <h2>Tic Tac Toe Game</h2>
+  </div>
+)
+
+export default TicTacToe;
