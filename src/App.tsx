0e587d45c546dc6d685d6b0207d8c666bbdcaf0a
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-03-24 14:42:42 +0900

    トップメニューを追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,35 @@ import Home from './Home';
 import Portfolio from './Portfolio';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import AppBar from 'material-ui/AppBar';
+import {List, ListItem} from 'material-ui/List';
 const logo = require('./logo.svg');
+const styles = {
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'row', // 横並びにする
+  },
+  linkInMenu: { // トップメニュー内のリンクのスタイル
+    textDecoration: 'none'  // 下線を消す
+  }
+};
 const App = () => (
   <MuiThemeProvider>
     <Router>
       <div className="App">
+        <AppBar
+          title="CodeComfort"
+        >
+          <List style={styles.flexContainer}>
+            <ListItem>
+              <Link to="/" style={styles.linkInMenu}>Home</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/portfolio" style={styles.linkInMenu}>Portfolio</Link>
+            </ListItem>
+            <ListItem primaryText="About"/>
+          </List>
+        </AppBar>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to CodeComfort Web Site!!!</h2>
@@ -17,7 +40,6 @@ const App = () => (
         <Route exact path="/" component={ Home }/>
         <Route path="/portfolio" component={ Portfolio }/>
       </div>
-
     </Router>
   </MuiThemeProvider>
 );
