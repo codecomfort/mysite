5040485decca9d78f5a91a384ffeff53939f47dc
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-03-30 23:59:28 +0900

    コメント修正

--- src/counter/Root.tsx
+++ src/counter/Root.tsx
@@ -4,11 +4,11 @@ import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
 import {ActionDispatcher} from './module';
 
-// 引数の Counter(これは Redux に依存しない純粋な React コンポーネント) に
-// store と actionDispatcher を差し込んで返すイメージ
-// このように DI することで、Counter コンポーネントは Redux に依存しなくてよくなる
-// また、index.tsx で Provider の store を設定していることで、
-// 右辺の store と dispatch は宣言することなく使用できる
+// 引数の Counter(これは Redux に依存しない純粋な React コンポーネント) に store と actionDispatcher を差し込んで返す
+// このように DI することで、Counter コンポーネントは Redux に依存しなくてよくなる Higher Order Components と呼ばれる手法
+// また、右辺の store と dispatch については index.tsx で Provider の store を設定していることで、
+// 宣言することなく使用できる
+// さらに、store.counter の counter は、Store.ts 内で宣言されている
 export default connect(
   (store: any) => ({value: store.counter}),
   (dispatch: Dispatch<any>) => ({actions: new ActionDispatcher(dispatch)})
