e072b254df6464ce7be119a7780f070343edc427
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-03-30 16:45:39 +0900

    Counterに元ネタをリンク

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -10,14 +10,18 @@ interface Props {
 // Root.tsx に Redux を切り出してあるので、Redux に依存しない実装ができる
 export class Counter extends React.Component<Props, {}> {
   render() {
-    return (
+    return <div>
       <div>
         <h2>Counter サンプル</h2>
         <p>score: {this.props.value.num}</p>
         <RaisedButton onClick={() => this.props.actions.increment(3)} label="Increment 3"/>
         <RaisedButton onClick={() => this.props.actions.decrement(2)} label="Decrement 2"/>
       </div>
-    )
+      <br />
+      <div>
+        <a href="http://qiita.com/uryyyyyyy/items/3ad88cf9ca9393335f8c">参考：React + Redux + TypeScriptの最小構成 - Qiita</a>
+      </div>
+    </div>
   }
 }
 
