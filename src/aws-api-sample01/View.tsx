bf421fe700b39cf8daff5bbb40ff7b61a87a4f6c
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-03-30 23:19:25 +0900

    Viewをアロー関数化

--- src/aws-api-sample01/View.tsx
+++ src/aws-api-sample01/View.tsx
@@ -3,6 +3,7 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import {InputArea} from './InputArea';
 import {JsonView} from './JsonView';
 import {ViewState, ActionDispatcher} from './module';
+import {StatelessComponent} from 'react';
 
 interface ViewProps {
   value: ViewState;
@@ -11,29 +12,22 @@ interface ViewProps {
 
 // ステートは Redux 管理とし、ViewState プロパティとして受け取る
 // 動作も ActionDispatcher で差し込む
-// →こうすることで、このコンポーネントは SFC にできる
-export default class View extends React.Component<ViewProps, {}> {
-  render() {
-    const {value, actions} = this.props;
+export const View: StatelessComponent<ViewProps> = (props) => <div>
+  <h2>AWS サンプル01</h2>
+  <InputArea
+    onChange={ props.actions.handleOnChange() }
+    onTouchTap={ props.actions.handleTouchTap(props.value.input) }
+  />
+  <Tabs>
+    <Tab label="JSON">
+      <JsonView recipes={props.value.jsonResult.recipes}/>
+    </Tab>
+    <Tab label="Preview">
+      <div>
+        プレビューデータ
+      </div>
+    </Tab>
+  </Tabs>;
+</div>;
 
-    return <div>
-      <h2>AWS サンプル01</h2>
-      <InputArea
-        onChange={ actions.handleOnChange() }
-        onTouchTap={ actions.handleTouchTap(value.input) }
-      />
-      <Tabs>
-        <Tab label="JSON">
-          <JsonView recipes={value.jsonResult.recipes}/>
-        </Tab>
-        <Tab label="Preview">
-          <div>
-            プレビューデータ
-          </div>
-        </Tab>
-      </Tabs>;
-    </div>
-  }
-}
-
-
+export default View;
