a91631d20e045d820e7d549b78635e542efd4c6b
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-12 00:24:52 +0900

    HomeページにPaperを3つ設置

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,24 @@
+import Paper from 'material-ui/Paper';
 import * as React from 'react';
 import {Link} from 'react-router-dom';
+import './Home.css';
 
 const logo = require('./logo.svg');
 const Home = () => (
   <div className="Home">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo"/>
-      <h2>Welcome to CodeComfort Web Site!!!</h2>
+    <div className="Home-eyecatch">
+      <img src={logo} className="Home-logo" alt="logo"/>
+      <h1>CodeComfort Web Site</h1>
+      <h2>コードで世界を快適に</h2>
     </div>
-    <h2>This is Home Page</h2>
+    <div>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    </div>
+    <div className="Paper-container">
+      <Paper className="Paper-style" zDepth={1}>aaa</Paper>
+      <Paper className="Paper-style" zDepth={1}>bbb</Paper>
+      <Paper className="Paper-style" zDepth={1}>ccc</Paper>
+    </div>
   </div>
 );
 
