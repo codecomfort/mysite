75550679148ab20862188c79fc571a6fe76c393b
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-11 13:31:24 +0900

    AppBarのデザインを変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import AppBar from 'material-ui/AppBar';
-import {List, ListItem} from 'material-ui/List';
+// import {List, ListItem} from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Portfolio from './Portfolio';
+import './css/font-icons.css';
 
 const logo = require('./logo.svg');
 
@@ -15,19 +17,11 @@ const App = () => (
       <div className="App">
         <AppBar
           className="App-bar"
-          title="CodeComfort"
-          showMenuIconButton={false}
-        >
-          <List className="Menu-list">
-            <ListItem>
-              <Link to="/" className="Link-menu">Home</Link>
-            </ListItem>
-            <ListItem>
-              <Link to="/portfolio" className="Link-menu">Portfolio</Link>
-            </ListItem>
-            <ListItem primaryText="About"/>
-          </List>
-        </AppBar>
+          iconElementRight={
+            <IconButton iconClassName="muidocs-icon-custom-github"
+                        href="https://github.com/codecomfort/"
+                        target="_blank"/> }
+        />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to CodeComfort Web Site!!!</h2>
