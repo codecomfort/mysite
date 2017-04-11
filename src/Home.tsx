fd5eb891865f4b33342bbc4182275e474677c87d
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-12 00:35:39 +0900

    マウスオーバーでPaperを浮かび上がらせる

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,22 +4,73 @@ import {Link} from 'react-router-dom';
 import './Home.css';
 
 const logo = require('./logo.svg');
-const Home = () => (
-  <div className="Home">
-    <div className="Home-eyecatch">
-      <img src={logo} className="Home-logo" alt="logo"/>
-      <h1>CodeComfort Web Site</h1>
-      <h2>コードで世界を快適に</h2>
-    </div>
-    <div>
-      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-    </div>
-    <div className="Paper-container">
-      <Paper className="Paper-style" zDepth={1}>aaa</Paper>
-      <Paper className="Paper-style" zDepth={1}>bbb</Paper>
-      <Paper className="Paper-style" zDepth={1}>ccc</Paper>
-    </div>
-  </div>
-);
+interface IHomeState {
+  paper1zDepth: number;
+  paper2zDepth: number;
+  paper3zDepth: number;
+}
 
-export default Home;
+export default class Home extends React.Component<{}, IHomeState> {
+  constructor() {
+    super();
+    this.state = {
+      paper1zDepth: 1,
+      paper2zDepth: 1,
+      paper3zDepth: 1,
+    };
+  }
+
+  public render() {
+    return (
+      <div className="Home">
+        <div className="Home-eyecatch">
+          <img src={logo} className="Home-logo" alt="logo"/>
+          <h1>CodeComfort Web Site</h1>
+          <h2>コードで世界を快適に</h2>
+        </div>
+        <div>
+          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </div>
+        <div className="Paper-container">
+          <Paper className="Paper-style" zDepth={this.state.paper1zDepth}
+                 onMouseEnter={ () => this.setState({
+                   paper1zDepth: 2,
+                   paper2zDepth: this.state.paper2zDepth,
+                   paper3zDepth: this.state.paper3zDepth,
+                 })}
+                 onMouseLeave={ () => this.setState({
+                   paper1zDepth: 1,
+                   paper2zDepth: this.state.paper2zDepth,
+                   paper3zDepth: this.state.paper3zDepth,
+                 })}
+          >aaa</Paper>
+          <Paper className="Paper-style" zDepth={this.state.paper2zDepth}
+                 onMouseEnter={ () => this.setState({
+                   paper1zDepth: this.state.paper1zDepth,
+                   paper2zDepth: 2,
+                   paper3zDepth: this.state.paper3zDepth,
+                 })}
+                 onMouseLeave={ () => this.setState({
+                   paper1zDepth: this.state.paper1zDepth,
+                   paper2zDepth: 1,
+                   paper3zDepth: this.state.paper3zDepth,
+                 })}
+          >bbb</Paper>
+          <Paper className="Paper-style" zDepth={this.state.paper3zDepth}
+                 onMouseEnter={ () => this.setState({
+                   paper1zDepth: this.state.paper1zDepth,
+                   paper2zDepth: this.state.paper2zDepth,
+                   paper3zDepth: 2,
+                 })}
+                 onMouseLeave={ () => this.setState({
+                   paper1zDepth: this.state.paper1zDepth,
+                   paper2zDepth: this.state.paper2zDepth,
+                   paper3zDepth: 1,
+                 })}
+          >ccc</Paper>
+        </div>
+      </div>
+    );
+  }
+}
+
