4715d2c62c3feacea3fdb32594d63f6b1ea37637
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-11 17:02:58 +0900

    ドロワーメニューを設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import AppBar from 'material-ui/AppBar';
-// import {List, ListItem} from 'material-ui/List';
+import Drawer from 'material-ui/Drawer';
+import {List, ListItem} from 'material-ui/List';
 import IconButton from 'material-ui/IconButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as React from 'react';
@@ -11,26 +12,56 @@ import './css/font-icons.css';
 
 const logo = require('./logo.svg');
 
-const App = () => (
-  <MuiThemeProvider>
-    <Router>
-      <div className="App">
-        <AppBar
-          className="App-bar"
-          iconElementRight={
-            <IconButton iconClassName="muidocs-icon-custom-github"
-                        href="https://github.com/codecomfort/"
-                        target="_blank"/> }
-        />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to CodeComfort Web Site!!!</h2>
-        </div>
-        <Route exact path="/" component={ Home }/>
-        <Route path="/portfolio" component={ Portfolio }/>
-      </div>
-    </Router>
-  </MuiThemeProvider>
-);
+interface IAppState {
+  drawerOpened: boolean;
+}
 
-export default App;
+export default class App extends React.Component<{}, IAppState> {
+  constructor() {
+    super();
+    this.state = {
+      drawerOpened: false,
+    };
+  }
+
+  public render() {
+    return (
+      <MuiThemeProvider>
+        <Router>
+          <div className="App">
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.drawerOpened}
+              onRequestChange={ (isOpened) =>
+                this.setState({
+                  drawerOpened: isOpened,
+                })}>
+              <div className="Menu-title">CodeComfort</div>
+              <List className="Menu-list">
+                <ListItem><Link to="/" className="Link-menu">Home</Link></ListItem>
+                <ListItem><Link to="/portfolio" className="Link-menu">Portfolio</Link>
+                </ListItem><ListItem primaryText="About"/>
+              </List>
+            </Drawer>
+            <AppBar
+              className="App-bar"
+              iconElementRight={
+                <IconButton iconClassName="muidocs-icon-custom-github"
+                            href="https://github.com/codecomfort/"
+                            target="_blank"/> }
+              onLeftIconButtonTouchTap={ (isOpened) => this.setState({drawerOpened: isOpened})}
+            />
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h2>Welcome to CodeComfort Web Site!!!</h2>
+            </div>
+            <Route exact path="/" component={ Home }/>
+            <Route path="/portfolio" component={ Portfolio }/>
+          </div>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
+}
+
