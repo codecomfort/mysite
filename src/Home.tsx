1d0e3218ac0bf0438cf88f8828c46f2d591284d1
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-12 22:28:09 +0900

    本家に倣ってPaperを3つ配置

--- src/Home.tsx
+++ src/Home.tsx
@@ -31,19 +31,25 @@ export default class Home extends React.Component<{}, IHomeState> {
         <div className="Home-purpose">
           <p>いいコードは、世界を快適にします。いいコードは、開発者も快適にします。いいコードを書こう。</p>
         </div>
-        <div className="Paper-container">
+        <div className="Paper-container clearfix">
           <Paper className="Paper-style" zDepth={this.state.paper1zDepth}
                  onMouseEnter={ () => this.setState({paper1zDepth: 2})}
-                 onMouseLeave={ () => this.setState({paper1zDepth: 1})}
-          >aaa</Paper>
+                 onMouseLeave={ () => this.setState({paper1zDepth: 1})}>
+            <h3 className="Paper-header">Portfolio</h3>
+            <div className="Paper-body">画像</div>
+          </Paper>
           <Paper className="Paper-style" zDepth={this.state.paper2zDepth}
                  onMouseEnter={ () => this.setState({paper2zDepth: 2})}
-                 onMouseLeave={ () => this.setState({paper2zDepth: 1})}
-          >bbb</Paper>
+                 onMouseLeave={ () => this.setState({paper2zDepth: 1})}>
+            <h3 className="Paper-header">About</h3>
+            <div className="Paper-body">画像</div>
+          </Paper>
           <Paper className="Paper-style" zDepth={this.state.paper3zDepth}
                  onMouseEnter={ () => this.setState({paper3zDepth: 2})}
-                 onMouseLeave={ () => this.setState({paper3zDepth: 1})}
-          >ccc</Paper>
+                 onMouseLeave={ () => this.setState({paper3zDepth: 1})}>
+            <h3 className="Paper-header">その他</h3>
+            <div className="Paper-body">画像</div>
+          </Paper>
         </div>
       </div>
     );
