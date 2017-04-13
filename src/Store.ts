331f84629f5565fff82aefd927863c9c357b5194
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-13 23:37:02 +0900

    Storeの初期値をImmutable.Mapと明示

--- src/Store.ts
+++ src/Store.ts
@@ -1,11 +1,14 @@
+import * as Im from 'immutable';
 import {createStore} from 'redux';
 import {combineReducers} from 'redux-immutable';
 import awsApiSample01Reducer from './aws-api-sample01/module';
 import counterReducer from './counter/module';  // default export に counterReducer と命名
 
+const initialState: Im.Map<string, any> = Im.Map({});
 export default createStore(
   combineReducers({
     awsApiSample01: awsApiSample01Reducer,
     counter: counterReducer,
   }),
+  initialState,
 );
