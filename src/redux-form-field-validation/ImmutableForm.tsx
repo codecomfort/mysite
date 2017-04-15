d1c9c72472cf0e839b66395f9d83a7689f34465d
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-15 16:52:15 +0900

    バリデーションにメールチェックと年齢チェックを追加

--- src/redux-form-field-validation/ImmutableForm.tsx
+++ src/redux-form-field-validation/ImmutableForm.tsx
@@ -27,7 +27,7 @@ const RenderField = (props: IFieldProps) => {
 }
 
 interface IFormProps {
-  // props のドキュメントにあるとおり、これらの項目は全て redux-form によりセットされる
+  // props のドキュメントにあるとおり、これらの項目は全て redux-form によりセットされる。
   handleSubmit: () => void;
   pristine: boolean;  // 初期状態
   reset: () => void;
@@ -39,10 +39,11 @@ export const ImmutableForm = (props: IFormProps) => (
     {/* Field の属性のうち、name と component は必須、他は開発者任意 */}
     <Field name="username" type="text" component={ RenderField } label="Username"/>
     <Field name="email" type="email" component={ RenderField } label="Email"/>
+    <p>(メールアドレス形式が不正な場合、送信ボタン押下のタイミングで、ブラウザによる警告も表示される場合があります)</p>
     <Field name="age" type="number" component={ RenderField } label="Age"/>
     <div>
-      <button type="submit" disabled={props.submitting}>Submit</button>
-      <button type="button" disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</button>
+      <button type="submit" disabled={props.submitting}>送信</button>
+      <button type="button" disabled={props.pristine || props.submitting} onClick={props.reset}>クリア</button>
     </div>
   </form>
 );
--- src/redux-form-field-validation/validate.ts
+++ src/redux-form-field-validation/validate.ts
@@ -1,10 +1,41 @@
-const validate = (values: any) => {
-  const errors: any = {};
+import * as Im from 'immutable';
+
+interface IValitationErros {
+  age: string;
+  email: string;
+  username: string;
+}
+
+// redux-form/immutable の reducer を使った場合、values はプレーンオブジェクトではなく
+// Immutable.Map 型で渡ってくる
+const validate = (values: Im.Map<string, any>): IValitationErros => {
+  const errors: IValitationErros = {
+    age: '',
+    email: '',
+    username: '',
+  };
+  const suffix: string = '(Client Validation)';
+
   if (!values.get('username')) {
-    errors.username = 'Required';
+    errors.username = '必須です' + suffix;
   } else if (values.get('username').length > 15) {
-    errors.username = 'Must be 15 characters or less';
+    errors.username = '15 文字以下で入力してください' + suffix;
   }
+
+  if (!values.get('email')) {
+    errors.email = '必須です' + suffix;
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.get('email'))) {
+    errors.email = '妥当なメールアドレスを入力してください' + suffix;
+  }
+
+  if (!values.get('age')) {
+    errors.age = '必須です' + suffix;
+  } else if (isNaN(Number(values.get('age')))) {
+    errors.age = '数値を入力してください' + suffix;
+  } else if (Number(values.get('age')) < 18) {
+    errors.age = '18 才以下はご遠慮ください' + suffix;
+  }
+
   return errors;
 };
 
