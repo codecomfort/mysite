18498359688635ca45deff4b7b81d4cf4e7dd79b
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-18 12:28:09 +0900

    emailバリデーションを修正
    入力しないでフォーカスを外してもバリデーションされてしまう問題へ対応

--- src/redux-form-material-ui/asyncValidate.ts
+++ src/redux-form-material-ui/asyncValidate.ts
@@ -1,14 +1,7 @@
 import * as Im from 'immutable';
 
-interface IValitationErros {
-  age: string;
-  email: string;
-  username: string;
-}
-
 const asyncValidate = async (values: Im.Map<string, any>) => {
   const email = values.get('email');
-  // FIXME 空のままフォーカスを外した場合はチェックしたくない
   await validateEmail(email);
 };
 
@@ -16,13 +9,15 @@ const sleep = (ms: number): Promise<void> => new Promise<void>((resolve) => setT
 
 const validateEmail = async (email) => {
   await sleep(3000); // メールアドレスの存在確認に時間がかかるエミュレート
-  if (!['foo@foo.com', 'bar@bar.com'].includes(email)) {
+  if (email && !['foo@foo.com', 'bar@bar.com'].includes(email)) {
     // Async Validation の場合、Sync Validation のように
     // errors をリターンするのでは無く、throw で通知する
     throw {
       email: '登録されていないアドレスです',
     };
   }
+
+  return {};
 };
 
 export default asyncValidate;
